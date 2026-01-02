96774ab08a58d5b575ae3e9a742cdb2a202ddb34
unknown <eng.ahmad.alassaf@gmail.com>
2026-01-02 19:50:11 +0100

    create main docker-compose file and docker-compose file for development and another one for production

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ const colors=require("colors");
 const path=require('path')
 const dotenv=require('dotenv').config({path:path.resolve(__dirname,'../.env')})
 const app=express();
-app.get('/',(req,res)=>res.send(`<h1>Hello!</></br><p>Server running on Port:${process.env.PORT}</br> NODE_ENV ${process.env.NODE_ENV}</p>`))
-app.listen(process.env.PORT,()=>console.log(`App running on Port: ${process.env.PORT}`.green.underline))
+app.get('/',(req,res)=>res.send(`<h1>Hello </h1></br> NODE_ENV ${process.env.NODE_ENV}</br><p>Server running on Port:${process.env.PORT}</br> </p>`))
+app.listen(process.env.PORT,()=>console.log(`App running on Port: ${process.env.PORT}!!`.green.underline))
