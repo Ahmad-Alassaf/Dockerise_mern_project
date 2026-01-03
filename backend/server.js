69f9bb68a997b18ec5241716f650c15f8bf67716
unknown <eng.ahmad.alassaf@gmail.com>
2026-01-03 11:49:50 +0100

    secondly approach to start app in dev/prod environment using multistag in docker-compose we using target:development or production

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ const colors=require("colors");
 const path=require('path')
 const dotenv=require('dotenv').config({path:path.resolve(__dirname,'../.env')})
 const app=express();
-app.get('/',(req,res)=>res.send(`<h1>Hello</h1></br> NODE_ENV ${process.env.NODE_ENV}!!!</br><p>Server running on Port:${process.env.PORT}</br> </p>`))
+app.get('/',(req,res)=>res.send(`<h1>Hello Multi stage </h1></br> NODE_ENV ${process.env.NODE_ENV}!!!</br><p>Server running on Port:${process.env.PORT}</br> </p>`))
 app.listen(process.env.PORT,()=>console.log(`App running on Port: ${process.env.PORT}!!`.green.underline))
