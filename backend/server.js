601b8b10d1df98bc59a5c2c38c4478a178828200
unknown <eng.ahmad.alassaf@gmail.com>
2026-01-04 20:55:55 +0100

    connect backend container to cloude Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
 const express=require("express")
 const colors=require("colors");
+const mongoose=require('mongoose')
 const path=require('path')
 const dotenv=require('dotenv').config({path:path.resolve(__dirname,'../.env')})
+let collections=[]
+const connectDataBase=async()=>{
+    try {
+      const conn=await  mongoose.connect(process.env.MONGOOSE_URI)
+      console.log(`Server connected with Data Base :${conn.connection.host}`.cyan.underline)
+        collections = await conn.connection.db.listCollections().toArray();
+        
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+        
+    }
+}
+// Connect to Database
+connectDataBase();
 const app=express();
+app.get('/database',(req,res)=>{
+    res.send(collections.map(c => c.name));
+})
 app.get('/',(req,res)=>res.send(`<h1>Hello Multi stage </h1></br> NODE_ENV ${process.env.NODE_ENV}!!!</br><p>Server running on Port:${process.env.PORT}</br> </p>`))
 app.listen(process.env.PORT,()=>console.log(`App running on Port: ${process.env.PORT}!!`.green.underline))
